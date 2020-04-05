20e9a00928c13ce20b889ceda26465f6b21868ec
NARJAM <n4rj4m@gmail.com>
2020-04-05 16:52:44 +0530

    Init

---
+++ FlowManager/FlowManager/FlowManager/App_Start/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Owin;
+using Microsoft.Owin;
+[assembly: OwinStartup(typeof(WebApplication4.Startup))]
+
+namespace WebApplication4
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            Microsoft.AspNet.SignalR.HubConfiguration config = new Microsoft.AspNet.SignalR.HubConfiguration();
+            config.EnableJSONP = false;
+            config.EnableJavaScriptProxies = false;
+            app.MapSignalR(config);
+   
+        }
+        
+    }
+}
---
+++ FlowManager/FlowManager/FlowManager/Hubs/FlowHub.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+using System.Threading.Tasks;
+
+namespace AccountManager.Hubs
+{ 
+    [HubName("flow")]
+    public class FlowHub : Hub
+    {
+        public override Task OnConnected()
+        {
+            string gameAuth = Context.QueryString["gameAuth"];
+
+            if (gameAuth == "Server")
+            {
+                Groups.Add(Context.ConnectionId, "servers");
+            }
+            else
+            {
+                Groups.Add(Context.ConnectionId, "clients");
+            }
+
+            Clients.Caller.OnSelfJoined(Context.ConnectionId, gameAuth);
+
+            Clients.Others.OnSessionJoined(Context.ConnectionId, gameAuth);
+            
+            return base.OnConnected();
+        }
+        
+        public override Task OnDisconnected(bool stopCalled)
+        {
+                Groups.Remove(Context.ConnectionId, "servers");
+                Groups.Remove(Context.ConnectionId, "clients");
+                Clients.All.OnSessionLeft(Context.ConnectionId);
+
+            return base.OnDisconnected(stopCalled);
+        }
+
+        public void SendToServer(string eventName, string eventData)
+        {
+            Clients.Group("servers").ReceiveFromClient(eventName, Context.ConnectionId, eventData);
+        }
+
+        public void SendToClients(string eventName, string eventData)
+        { 
+            Clients.Group("clients").ReceiveFromServer(eventName, Context.ConnectionId, eventData);
+        }
+        
+        public void ErrorLog(string message)
+        {
+            Clients.All.ErrorReceived(message);
+        }
+    }
+}
